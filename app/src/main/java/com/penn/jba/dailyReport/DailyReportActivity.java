94726c9d89d3a73b2fa382c6923ccda4e91d0e17
Raighne Weng <raighne.weng@gmail.com>
2017-05-18 00:12:51 +0800

    temp save may 17 night

--- app/src/main/java/com/penn/jba/dailyReport/DailyReportActivity.java
+++ app/src/main/java/com/penn/jba/dailyReport/DailyReportActivity.java
@@ -1,22 +1,19 @@
 package com.penn.jba.dailyReport;
 
+import android.support.v4.app.FragmentManager;
 import android.content.Context;
 import android.databinding.DataBindingUtil;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebViewFragment;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.github.florent37.materialviewpager.MaterialViewPager;
-import com.github.florent37.materialviewpager.header.HeaderDesign;
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.penn.jba.R;
 import com.penn.jba.databinding.ActivityDailyReportBinding;
@@ -34,6 +31,7 @@ import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
@@ -45,7 +43,6 @@ import io.reactivex.schedulers.Schedulers;
 import io.realm.Realm;
 
 import static android.R.attr.key;
-import static com.penn.jba.R.id.materialViewPager;
 import static com.penn.jba.util.PPHelper.ppWarning;
 
 public class DailyReportActivity extends AppCompatActivity {
@@ -58,7 +55,7 @@ public class DailyReportActivity extends AppCompatActivity {
     //custom
     private String dailyReportId;
 
-    private ArrayList<String> profilePics = new ArrayList();
+    private List<DailyReportFragment> fragments;
 
     private String fans;
     private String collects;
@@ -100,7 +97,18 @@ public class DailyReportActivity extends AppCompatActivity {
     }
 
     private void setup() {
-        //获取dailyReport数据
+        binding.tl.setTitle(R.string.daily_report_title);
+        setSupportActionBar(binding.tl);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        initData();
+
+    }
+
+    private void initData() {
+        // 获取dailyReport数据
         PPJSONObject jBodyFans = new PPJSONObject();
         jBodyFans
                 .put("id", dailyReportId)
@@ -141,22 +149,13 @@ public class DailyReportActivity extends AppCompatActivity {
                                        @Override
                                        public void accept(String s) throws Exception {
                                            //pptodo 这里如果有ppwarn的话需要抛出exception
-                                           Log.v("pplog121", s);
-                                           Log.v("pplog121", "pptest:" + PPHelper.ppFromString(s, "data.detail.fans"));
                                            if (PPHelper.ppFromString(s, "data.detail.fans") != null) {
-                                               Log.v("pplog121", "accept1:");
                                                fans = PPHelper.ppFromString(s, "data.detail.fans").getAsJsonArray().toString();
-                                               Log.v("pplog121", "accept2:");
-                                               Log.v("pplog121", "accept:" + fans);
                                            } else if (PPHelper.ppFromString(s, "data.detail.collects") != null) {
                                                collects = PPHelper.ppFromString(s, "data.detail.collects").getAsJsonArray().toString();
-                                               Log.v("pplog121", "accept:" + collects);
                                            } else if (PPHelper.ppFromString(s, "data.detail.beCollecteds") != null) {
                                                beCollecteds = PPHelper.ppFromString(s, "data.detail.beCollecteds").getAsJsonArray().toString();
-                                               Log.v("pplog121", "accept:" + beCollecteds);
                                            }
-
-                                           Log.v("pplog121", "end");
                                            if (++count == 3) {
                                                //请求全部结束
                                                setupPage();
@@ -169,143 +168,177 @@ public class DailyReportActivity extends AppCompatActivity {
                                         Log.v("pplog102", "error102:" + t);
                                     }
                                 }));
-
-        try (Realm realm = Realm.getDefaultInstance()) {
-            CurrentUser currentUser = realm.where(CurrentUser.class).findFirst();
-            Picasso.with(activityContext)
-                    .load(PPHelper.get80ImageUrl(currentUser.getHead()))
-                    .placeholder(R.drawable.pictures_no)
-                    .into((ImageView) binding.materialViewPager.findViewById(R.id.avatar_iv));
-            int size = currentUser.getPics().size();
-            if (size == 0) {
-                //pptodo 替换成七牛上的默认图片
-                profilePics.add("http://phandroid.s3.amazonaws.com/wp-content/uploads/2014/06/android_google_moutain_google_now_1920x1080_wallpaper_Wallpaper-HD_2560x1600_www.paperhi.com_-640x400.jpg");
-            }
-            for (int i = 0; i < size; i++) {
-                profilePics.add(currentUser.getPics().get(i).getNetFileName());
-            }
-        }
     }
 
     //-----helper-----
     private void setupPage() {
-        Log.v("pplog121", "setupPage");
-        binding.materialViewPager.getViewPager().setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+        fragments = new ArrayList<DailyReportFragment>();
 
-            @Override
-            public Fragment getItem(int position) {
-                Log.v("pplog121", "getItem");
-                switch (position) {
-                    case 0:
-                        Log.v("pplog121", "pptest1:" + beCollecteds);
-                        beCollectedsJsonArray = PPHelper.ppFromString(beCollecteds, "").getAsJsonArray();
-                        return ReportListFragment.newInstance("beCollecteds", beCollecteds);
-                    case 1:
-                        Log.v("pplog121", "pptest2:" + collects);
-                        return ReportListFragment.newInstance("collects", collects);
-                    case 2:
-                        Log.v("pplog121", "pptest3:" + fans);
-                        return ReportListFragment.newInstance("fans", fans);
-                }
+        JsonArray fansData = new Gson().fromJson(fans, JsonArray.class);
+        JsonArray collectsData = new Gson().fromJson(collects, JsonArray.class);
+        JsonArray beCollectedsData = new Gson().fromJson(beCollecteds, JsonArray.class);
 
-                return null;
-            }
+        if (beCollectedsData.size() != 0) {
+            fragments.add(DailyReportFragment.newInstance("beCollecteds", beCollecteds));
+        }
+        if (collectsData.size() != 0) {
+            fragments.add(DailyReportFragment.newInstance("collects", collects));
+        }
+        if (fansData.size() != 0) {
+            fragments.add(DailyReportFragment.newInstance("fans", fans));
+        }
 
-            @Override
-            public int getCount() {
-                return 3;
-            }
+        MyPagerAdapter myFragmentPagerAdapter = new MyPagerAdapter(getSupportFragmentManager(), fragments);
+        binding.mainVp.setAdapter(myFragmentPagerAdapter);
+        binding.mainVp.setOffscreenPageLimit(3);
 
-            @Override
-            public CharSequence getPageTitle(int position) {
-                switch (position) {
-                    case 0:
-                        return "被迹录";
-                    case 1:
-                        return "迹录";
-                    case 2:
-                        return "新粉丝";
-                }
-                return "";
-            }
-        });
+        binding.loadingPb.setVisibility(View.INVISIBLE);
+    }
 
-        Log.v("pplog121", "setupPage2");
+    class MyPagerAdapter extends FragmentPagerAdapter {
+        private List<DailyReportFragment> fragments;
 
-        binding.materialViewPager.setMaterialViewPagerListener(new MaterialViewPager.Listener() {
-            @Override
-            public HeaderDesign getHeaderDesign(int page) {
-                switch (page) {
-                    case 0:
-                        return HeaderDesign.fromColorResAndUrl(
-                                R.color.green,
-                                PPHelper.get800ImageUrl(profilePics.get(0 % profilePics.size()))
-                        );
-                    case 1:
-                        return HeaderDesign.fromColorResAndUrl(
-                                R.color.blue,
-                                PPHelper.get800ImageUrl(profilePics.get(1 % profilePics.size()))
-                        );
-                    case 2:
-                        return HeaderDesign.fromColorResAndUrl(
-                                R.color.cyan,
-                                PPHelper.get800ImageUrl(profilePics.get(2 % profilePics.size()))
-                        );
-                }
+        public MyPagerAdapter(FragmentManager fm, List<DailyReportFragment> fragments) {
+            super(fm);
+            this.fragments = fragments;
 
-                return null;
-            }
-        });
+        }
 
-        Log.v("pplog121", "setupPage7");
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+//            switch (position) {
+//                case 0:
+//                    return DailyReportFragment.newInstance("fans", fans);
+//
+//            }
+            return fragments.get(position);
+        }
 
-        binding.materialViewPager.getViewPager().setOffscreenPageLimit(binding.materialViewPager.getViewPager().getAdapter().getCount());
-        binding.materialViewPager.getPagerTitleStrip().setViewPager(binding.materialViewPager.getViewPager());
-
-        Log.v("pplog121", "setupPage8");
-
-        binding.materialViewPager.getViewPager().addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                if (position == 0) {
-                    setSubTitle("今天我被" + beCollectedsJsonArray.size() + "人迹录了片刻");
-                } else if (position == 1) {
-                    JsonArray ja = PPHelper.ppFromString(collects, "").getAsJsonArray();
-                    setSubTitle("今天我迹录了" + ja.size() + "人片刻");
-                } else if (position == 2) {
-                    JsonArray ja = PPHelper.ppFromString(fans, "").getAsJsonArray();
-                    setSubTitle("今天我增加了" + ja.size() + "个新粉丝");
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
-        Log.v("pplog121", "setupPage9");
-
-        setSubTitle("今天我被" + beCollectedsJsonArray.size() + "人迹录了片刻");
-        Log.v("pplog121", "setupPage10");
-
-        Toolbar toolbar = binding.materialViewPager.getToolbar();
-        if (toolbar != null) {
-            setSupportActionBar(toolbar);
-
-            final ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.setDisplayHomeAsUpEnabled(true);
-            }
+        @Override
+        public int getCount() {
+            return fragments.size();
         }
     }
-
-    private void setSubTitle(String str) {
-        ((TextView) binding.materialViewPager.findViewById(R.id.main_tv)).setText(str);
-    }
+//
+//    //-----helper-----
+//    private void setupPage() {
+//        Log.v("pplog121", "setupPage");
+//        binding.materialViewPager.getViewPager().setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+//
+//            @Override
+//            public Fragment getItem(int position) {
+//                Log.v("pplog121", "getItem");
+//                switch (position) {
+//                    case 0:
+//                        Log.v("pplog121", "pptest1:" + beCollecteds);
+//                        beCollectedsJsonArray = PPHelper.ppFromString(beCollecteds, "").getAsJsonArray();
+//                        return ReportListFragment.newInstance("beCollecteds", beCollecteds);
+//                    case 1:
+//                        Log.v("pplog121", "pptest2:" + collects);
+//                        return ReportListFragment.newInstance("collects", collects);
+//                    case 2:
+//                        Log.v("pplog121", "pptest3:" + fans);
+//                        return ReportListFragment.newInstance("fans", fans);
+//                }
+//
+//                return null;
+//            }
+//
+//            @Override
+//            public int getCount() {
+//                return 3;
+//            }
+//
+//            @Override
+//            public CharSequence getPageTitle(int position) {
+//                switch (position) {
+//                    case 0:
+//                        return "被迹录";
+//                    case 1:
+//                        return "迹录";
+//                    case 2:
+//                        return "新粉丝";
+//                }
+//                return "";
+//            }
+//        });
+//
+//        Log.v("pplog121", "setupPage2");
+//
+//        binding.materialViewPager.setMaterialViewPagerListener(new MaterialViewPager.Listener() {
+//            @Override
+//            public HeaderDesign getHeaderDesign(int page) {
+//                switch (page) {
+//                    case 0:
+//                        return HeaderDesign.fromColorResAndUrl(
+//                                R.color.green,
+//                                PPHelper.get800ImageUrl(profilePics.get(0 % profilePics.size()))
+//                        );
+//                    case 1:
+//                        return HeaderDesign.fromColorResAndUrl(
+//                                R.color.blue,
+//                                PPHelper.get800ImageUrl(profilePics.get(1 % profilePics.size()))
+//                        );
+//                    case 2:
+//                        return HeaderDesign.fromColorResAndUrl(
+//                                R.color.cyan,
+//                                PPHelper.get800ImageUrl(profilePics.get(2 % profilePics.size()))
+//                        );
+//                }
+//
+//                return null;
+//            }
+//        });
+//
+//        Log.v("pplog121", "setupPage7");
+//
+//        binding.materialViewPager.getViewPager().setOffscreenPageLimit(binding.materialViewPager.getViewPager().getAdapter().getCount());
+//        binding.materialViewPager.getPagerTitleStrip().setViewPager(binding.materialViewPager.getViewPager());
+//
+//        Log.v("pplog121", "setupPage8");
+//
+//        binding.materialViewPager.getViewPager().addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//
+//            }
+//
+//            @Override
+//            public void onPageSelected(int position) {
+//                if (position == 0) {
+//                    setSubTitle("今天我被" + beCollectedsJsonArray.size() + "人迹录了片刻");
+//                } else if (position == 1) {
+//                    JsonArray ja = PPHelper.ppFromString(collects, "").getAsJsonArray();
+//                    setSubTitle("今天我迹录了" + ja.size() + "人片刻");
+//                } else if (position == 2) {
+//                    JsonArray ja = PPHelper.ppFromString(fans, "").getAsJsonArray();
+//                    setSubTitle("今天我增加了" + ja.size() + "个新粉丝");
+//                }
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//
+//            }
+//        });
+//
+//        Log.v("pplog121", "setupPage9");
+//
+//        setSubTitle("今天我被" + beCollectedsJsonArray.size() + "人迹录了片刻");
+//        Log.v("pplog121", "setupPage10");
+//
+//        Toolbar toolbar = binding.materialViewPager.getToolbar();
+//        if (toolbar != null) {
+//            setSupportActionBar(toolbar);
+//
+//            final ActionBar actionBar = getSupportActionBar();
+//            if (actionBar != null) {
+//                actionBar.setDisplayHomeAsUpEnabled(true);
+//            }
+//        }
+//    }
+//
+//    private void setSubTitle(String str) {
+//        ((TextView) binding.materialViewPager.findViewById(R.id.main_tv)).setText(str);
+//    }
 }
---
+++ app/src/main/java/com/penn/jba/dailyReport/DailyReportFragment.java
@@ -0,0 +1,123 @@
+package com.penn.jba.dailyReport;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.penn.jba.R;
+import com.penn.jba.databinding.DailyReportFgmtBinding;
+import com.penn.jba.model.realm.CurrentUser;
+import com.penn.jba.util.PPHelper;
+import com.penn.jba.view.MyDecoration;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import io.reactivex.disposables.Disposable;
+import io.realm.Realm;
+
+/**
+ * Created by raighne on 5/17/17.
+ */
+
+public class DailyReportFragment extends Fragment {
+    private Context activityContext;
+
+    private ArrayList<Disposable> disposableList = new ArrayList<Disposable>();
+
+    private Realm realm;
+    private CurrentUser currentUser;
+
+    private String type;
+    private String data = "";
+
+    private DailyReportFgmtBinding binding;
+
+    public DailyReportFragment() {
+        // Required empty public constructor
+    }
+
+    public static DailyReportFragment newInstance(String type, String data) {
+        DailyReportFragment fragment = new DailyReportFragment();
+        Bundle args = new Bundle();
+        args.putString("type", type);
+        args.putString("data", data);
+        fragment.setArguments(args);
+
+        Log.d("weng090", type);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityContext = getActivity();
+        if (getArguments() != null) {
+            type = getArguments().getString("type");
+            data = getArguments().getString("data");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //common
+        binding = DataBindingUtil.inflate(
+                inflater, R.layout.daily_report_fgmt, container, false);
+        View view = binding.getRoot();
+        binding.setPresenter(this);
+        //end common
+
+        setup();
+
+        return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        realm.close();
+        for (Disposable d : disposableList) {
+            if (!d.isDisposed()) {
+                d.dispose();
+            }
+        }
+    }
+
+    private void setup() {
+        String tmpHeader = "";
+        realm = Realm.getDefaultInstance();
+        currentUser = realm.where(CurrentUser.class).findFirst();
+        tmpHeader = currentUser.getBanner();
+
+        Picasso.with(activityContext)
+                .load(PPHelper.get80ImageUrl(tmpHeader))
+                .into(binding.headerCiv);
+
+        JsonArray tmpData = new Gson().fromJson(data, JsonArray.class);
+        if (type.equals("fans")) {
+            binding.headerTv1.setText("今天我增加了");
+            binding.headerTv2.setText(tmpData.size() + "个新粉丝");
+        } else if (type.equals("collects")) {
+            binding.headerTv1.setText("今天我迹录了");
+            binding.headerTv2.setText(tmpData.size() + "次他人片刻");
+        } else {
+            binding.headerTv1.setText("今天我被");
+            binding.headerTv2.setText(tmpData.size() + "人迹录了片刻");
+        }
+
+        binding.mainRv.setLayoutManager(new LinearLayoutManager(activityContext));
+        binding.mainRv.setHasFixedSize(true);
+        Log.v("pplog121", tmpData.toString());
+        binding.mainRv.addItemDecoration(new MyDecoration(activityContext));
+        binding.mainRv.setAdapter(new ReportListAdapter(activityContext, type, tmpData));
+    }
+}
